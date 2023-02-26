1ef2d634831fa4c270d8df28efe18f64cb4dfd28
Neroxn <e2380566@ceng.metu.edu.tr>
2023-02-26 17:46:46 +0300

    Add default logger to registry

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,9 +1,10 @@
-from .logger import TensorboardLogger, WandbLogger, ClearmlLogger
+from .logger import TensorboardLogger, WandbLogger, ClearmlLogger,PythonLogger
 
 LOGGER_REGISTRY = {
     "tensorboard": TensorboardLogger,
     "wandb": WandbLogger,
     "clearml": ClearmlLogger,
+    "default": PythonLogger
 }
 
 def create_logger(logger_config):
@@ -15,11 +16,13 @@ def create_logger(logger_config):
         logger (NetworkLogger): logger
     """
     if logger_config is None:
-        return None
-    logger_type = logger_config.pop("type")
-    if logger_type not in LOGGER_REGISTRY:
-        raise ValueError("Unknown logger type: {}".format(logger_type))
-    logger = LOGGER_REGISTRY[logger_type](**logger_config)
-
+        logger_type = "default"
+        logger = LOGGER_REGISTRY[logger_type]()
+    else:
+        logger_type = logger_config.pop("type")
+        if logger_type not in LOGGER_REGISTRY:
+            print("Unknown logger type: {}. Using default python logger".format(logger_type))
+            logger_type = "default"
+        logger = LOGGER_REGISTRY[logger_type](**logger_config)
     return logger
     
