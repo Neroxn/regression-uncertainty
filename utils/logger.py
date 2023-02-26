70216e688eec7050555080ee39c4818299311997
Neroxn <e2380566@ceng.metu.edu.tr>
2023-02-26 17:46:32 +0300

    Add track list to the logger for calculating stats

--- utils/logger.py
+++ utils/logger.py
@@ -64,6 +64,13 @@ class NetworkLogger(object):
 
     def __init__(self):
         self.track_list = {}
+
+    def reset_track_list(self, **kwargs):
+        """ Reset the track list. """
+        track_list = self.track_list
+        self.track_list = {}
+        return track_list
+    
     def log_metric(self, **kwargs):
         """ Log a metric using the logger."""
         raise NotImplementedError
@@ -72,16 +79,45 @@ class NetworkLogger(object):
         """ Initialize the logger. """
         raise NotImplementedError
 
+    def track(self, name, value, type = "last"):
+        """ Track a value. Since logger is initialized once, this tracks values across cv splits. """
+        if type == "last":
+            self.track_list[name] = [value]
+        elif type == "list":
+            if name not in self.track_list:
+                self.track_list[name] = []
+            self.track_list[name].append(value)
+
+class PythonLogger(NetworkLogger):
+    """ Use logger class to log network training. """
+    def __init__(self):
+        super().__init__()
+        self.init_logger()
+
+    def log_metric(self, metric_dict, step, print_log=True):
+        for k,v in metric_dict.items():
+            if print_log is True:
+                self.logger.info("Step: {} | {}: {}".format(step, k, v))
+            self.track(k,v,type="list")
+    def init_logger(self):
+        self.logger = set_logger()
+
+    def log_meta(self, meta_dict):
+        pretty_dict_print(meta_dict)
 
 class WandbLogger(NetworkLogger):
     """ Logger for logging network training to wandb. """
     def __init__(self, **kwargs):
+        super().__init__()
         if LOGGER_CHECK["wandb"] is False:
             raise ImportError("wandb is not installed")
         self.init_logger(**kwargs)
 
-    def log_metric(self, metric_dict, step):
+    def log_metric(self, metric_dict, step, **kwargs):
         wandb.log(metric_dict, step=step)
+        for k,v in metric_dict.items():
+            self.track(k,v,type="list")
+
 
     def init_logger(self, **kwargs):
         return wandb.init(**kwargs)
@@ -92,6 +128,7 @@ class WandbLogger(NetworkLogger):
 class ClearmlLogger(NetworkLogger):
     """ Logger for logging network training to clearml. """
     def __init__(self, **kwargs):
+        super().__init__()
         if LOGGER_CHECK["clearml"] is False:
             raise ImportError("clearml is not installed")
         self.init_logger(**kwargs)
@@ -100,10 +137,11 @@ class ClearmlLogger(NetworkLogger):
         self.task = clearml.Task.init(**kwargs)
         self.logger = self.task.get_logger()
 
-    def log_metric(self, metric_dict, step):
+    def log_metric(self, metric_dict, step, **kwargs):
         for k,v in metric_dict.items():
             self.logger.report_scalar(title=k, series=k, value=v, iteration=step)
-
+            self.track(k,v,type="list")
+    
     def log_matplotlib_figure(self, figure, figure_name, step):
         self.logger.report_matplotlib_figure(figure=figure, figure_name=figure_name, iteration=step)
         
@@ -113,6 +151,7 @@ class ClearmlLogger(NetworkLogger):
 class TensorboardLogger(NetworkLogger):
     """ Logger for logging network training to tensorboard. """
     def __init__(self, **kwargs):
+        super().__init__()
         if LOGGER_CHECK["tensorboard"] is False:
             raise ImportError("tensorboard is not installed")
         self.init_logger(**kwargs)
@@ -120,6 +159,7 @@ class TensorboardLogger(NetworkLogger):
     def init_logger(self, **kwargs):
         self.logger = SummaryWriter(**kwargs)
 
-    def log_metric(self, metric_dict, step):
+    def log_metric(self, metric_dict, step, **kwargs):
         for k, v in metric_dict.items():
             self.logger.add_scalar(k, v, step)
+            self.track(k,v,type="list")
