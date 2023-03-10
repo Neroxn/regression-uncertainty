ebf6a6eba305a473281e35808cc3b8665452572c
Neroxn <e2380566@ceng.metu.edu.tr>
2023-03-10 13:53:16 +0300

    Add modulatiy where models are transformed into classes recursively

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,16 @@
 import torch 
 from .base import BaseModel
 from .multi_layer import LinearVarianceNetworkHead, Conv2DVarianceNetworkHead
+from .resnet import BasicBlock, Bottleneck, ResNet
 from copy import deepcopy
 
 # Custom layer registery. Custom layers can be added to this registery.
 LAYER_REGISTERY = {
     "LinearVarianceNetworkHead": LinearVarianceNetworkHead,
     "Conv2DVarianceNetworkHead": Conv2DVarianceNetworkHead,
+    "BasicBlock" : BasicBlock,
+    "Bottleneck" : Bottleneck,
+    "ResNet" : ResNet,
 } 
 
 # Add all torch.nn layers to the registery
@@ -14,6 +18,17 @@ for name in dir(torch.nn):
     obj = getattr(torch.nn, name)
     if isinstance(obj, type) and issubclass(obj, torch.nn.Module):
         LAYER_REGISTERY[name] = obj
+
+def _replace_layer(value):
+    """
+    Replace a value that is an instance of list or str with the corresponding layer.
+    """
+    if value.__class__ == list:
+        return [_replace_layer(v) for v in value]
+    elif value.__class__ == str:
+        if value in LAYER_REGISTERY:
+            return LAYER_REGISTERY[value]
+    return value
 
 def create_model(model_config):
     """
@@ -25,6 +40,12 @@ def create_model(model_config):
     for layer_config in model_config:
         for layer_name, layer_args in layer_config.items():
             layer_class = layer_args.pop("class")
+
+            # layer_args might include another dict. Check recursively for possible block constructions
+            for key, value in layer_args.items():
+                if value.__class__ == dict and  value.get("class"): 
+                    layer_args[key] = create_model([value]) # construct the layer
+                layer_args[key] = _replace_layer(value) # replace the layer with the actual layer class
             layer = LAYER_REGISTERY[layer_class](**layer_args)
             base_model.add_layer(layer_name, layer)
     return base_model
